f1cd3bb7a823e119a6e7882fe29029096d9ae8de
yakim <yakimmm1983@yandex.ru>
2023-12-25 20:13:10 +0300

    #5 Исправлены ошибки запуска файла

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ def BoubleSort(arr:list) -> list:
 
 def Reverse(arr:list) -> list:
     for i in range(len(mass) // 2):
-    mass[i], mass[-1 - i] = mass[-1 - i], mass[i]
+        mass[i], mass[-1 - i] = mass[-1 - i], mass[i]
 
 
 
 
 if __name__ == "__main__":
     mass = [1,2,9,3,8,23,9,228,3]
-    mass = BoubleSort(mass)
+    mass1 = BoubleSort(mass)
     mass2 = Reverse(mass)
     mass.reverse()
-    if mass == mass2:
+    if mass1 == mass2:
         print("Ok")
     else:
         print("Not Work")
